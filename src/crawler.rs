6260c548818ec8eaa6e4f5314374de0c56c47028
Wojciech Sęk <wojciech.sek17@gmail.com>
2022-09-06 00:30:07 +0200

    added normalization

--- src/crawler.rs
+++ src/crawler.rs
@@ -1,3 +1,5 @@
+use super::graphs::Graph;
+use super::scraper::Scraper;
 
 pub struct Crawler {
     input: String,
@@ -22,11 +24,19 @@ impl Crawler {
     /// Crawl the web based on given url and max_depth.
     /// ```
     /// use bipartite::crawler::Crawler;
-    /// let links = Crawler::crawl();
-    /// assert_eq!(links, 4);
+    /// let crawler = Crawler::new("https://pwr.edu.pl/".to_owned(), 1, None);
+    /// let links = crawler.crawl();
+    /// assert!(true);
     /// ```
-    pub fn crawl() -> usize {
+    pub fn crawl(&self) -> Graph {
         let num_of_threads = num_cpus::get_physical();
-        num_of_threads
+        
+        let scrapers = {
+            let mut scrapers = Vec::with_capacity(num_of_threads);
+            (0..num_of_threads).into_iter().for_each(|_| scrapers.push(Scraper::new(self.must_contain.clone())));
+            scrapers
+        };      
+
+        Graph::empty()
     }
 }
--- src/scraper.rs
+++ src/scraper.rs
@@ -1,14 +1,15 @@
 extern crate reqwest;
 extern crate scraper;
 
-use std::collections::HashSet;
+use normalize_url::normalizer;
 use scraper::{Html, Selector};
+use std::collections::HashSet;
 
-/// A scraper for HTML documents finding links to other pages. 
+/// A scraper for HTML documents finding links to other pages.
 /// It contains an optional word that must be contained within the url address of found pages.
 pub struct Scraper {
-    client : reqwest::blocking::Client,
-    must_contain : Option<String>
+    client: reqwest::blocking::Client,
+    must_contain: Option<String>,
 }
 
 impl Scraper {
@@ -21,17 +22,17 @@ impl Scraper {
     /// ```
     pub fn new(must_contain: Option<String>) -> Scraper {
         Scraper {
-            client : reqwest::blocking::Client::new(),
-            must_contain
+            client: reqwest::blocking::Client::new(),
+            must_contain,
         }
     }
 
-    /// Scrapes the given url for links to other pages.
+    /// Scrapes the given url for links to other pages while normalizing their urls.
     /// # Examples
     /// ```
     /// use bipartite::scraper::Scraper;
     /// let scraper = Scraper::new(None);
-    /// let links = scraper.scrape("https://www.rust-lang.org/en-US/");
+    /// let links = scraper.scrape("https://pwr.edu.pl/");
     /// assert!(links.len() > 0);
     /// ```
     pub fn scrape(&self, url: &str) -> Vec<String> {
@@ -44,21 +45,55 @@ impl Scraper {
             match self.must_contain {
                 Some(ref word) => {
                     for link in document.select(&selector) {
-                        let value = link.value().attr("href").unwrap();
-                        if value.starts_with("http") && value.contains(word) {
-                            result.insert(value.to_string());
+                        let value = link.value().attr("href");
+                        match value {
+                            Some(href) => {
+                                if href.starts_with("http") && href.contains(word) {
+                                    let normalizer = normalizer::UrlNormalizer::new(href);
+                                    match normalizer {
+                                        Ok(normalizer) => {
+                                            let normalized = normalizer.normalize(None);
+                                            match normalized {
+                                                Ok(normalized) => {
+                                                    result.insert(normalized.to_owned());
+                                                }
+                                                Err(_) => {}
+                                            }
+                                        }
+                                        Err(_) => {}
+                                    }
+                                }
+                            }
+                            None => (),
                         }
                     }
                 }
                 None => {
                     for link in document.select(&selector) {
-                        let value = link.value().attr("href").unwrap();
-                        if value.starts_with("http") {
-                            result.insert(value.to_string());
+                        let value = link.value().attr("href");
+                        match value {
+                            Some(href) => {
+                                if href.starts_with("http") {
+                                    let normalizer = normalizer::UrlNormalizer::new(href);
+                                    match normalizer {
+                                        Ok(normalizer) => {
+                                            let normalized = normalizer.normalize(None);
+                                            match normalized {
+                                                Ok(normalized) => {
+                                                    result.insert(normalized.to_owned());
+                                                }
+                                                Err(_) => {}
+                                            }
+                                        }
+                                        Err(_) => {}
+                                    }
+                                }
+                            }
+                            None => (),
                         }
                     }
                 }
-            }               
+            }
         }
         result.into_iter().collect()
     }
