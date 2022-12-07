8db34f2a2978c714b51ac88427fbd9c332732632
Wojciech Sęk <wojciech.sek17@gmail.com>
2022-12-07 22:19:46 +0100

    main to check random graphs

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,21 @@ use bipartite::graphs::Graph;
 use bipartite::bipartite::{find_bipartite, qr_parameters};
 
 fn main() {
-    // Complete graphs
-    for n in (1..=10).map(|x| 1000 * x) {
-        let filename = format!("k_n{}.json", n);
-        let result_filename = format!("result_{}", filename);
-        let k_n = Graph::read_from_json(&filename);
-        let (q, r) = qr_parameters(&k_n);
-        let start = Instant::now();
-        let (set1, set2) = find_bipartite(&k_n, 2, 2);
-        let duration = start.elapsed();
-        let result = format!("duration={:?}, q={}, r={}, set1={}, set2={}", duration, q, r, set1.len(), set2.len());
-        std::fs::write(result_filename, result).expect("Unable to write file");
-        println!("Done {} in {:?}", filename, duration);
+    let current_dir = std::env::current_dir().unwrap();
+    for filename in std::fs::read_dir(current_dir).unwrap() {
+        if let Ok(name) = filename {
+            let name = name.file_name().into_string().unwrap();
+            if name.ends_with(".json") && name.starts_with("random") {
+                let result_filename = format!("result_{}", name);
+                let k_n = Graph::read_from_json(&name);
+                let (q, r) = qr_parameters(&k_n);
+                let start = Instant::now();
+                let (set1, set2) = find_bipartite(&k_n, 2, 2);
+                let duration = start.elapsed();
+                let result = format!("duration={:?}, q={}, r={}, set1={}, set2={}", duration, q, r, set1.len(), set2.len());
+                std::fs::write(result_filename, result).expect("Unable to write file");
+                println!("Done {} in {:?}", name, duration);
+            }
+        }
     }
 }
